4b0ad496efbf6927688833369a437ee02128ce6c
Angelo <angelo.dias@grupofolha.com.br>
2018-07-03 14:41:29 -0300

    Form google funcionando

--- src/assets/scripts/components/store/form.js
+++ src/assets/scripts/components/store/form.js
@@ -6,7 +6,7 @@ export default class Form extends Component {
 		const form = document.forms['submit-to-google-sheet'];
 		const origin = 'https://script.google.com/macros/s/AKfycbziqRYW3AMjypbywbj75cWWH-ODeRg0nj_vyNZYU4Ap3PSYEis/exec';
 
-		function toJSONString(f) {
+		function formatter(f) {
 			const obj = {};
 			const elements = f.querySelectorAll('input, select, textarea');
 			for (let i = 0; i < elements.length; i += 1) {
@@ -17,18 +17,18 @@ export default class Form extends Component {
 					obj[name] = value;
 				}
 			}
-			return JSON.stringify(obj);
+			return obj;
 		}
 
 		form.addEventListener('submit', (e) => {
 			e.preventDefault();
-			const data = toJSONString(form);
-			axios.post(origin, data)
+			const data = formatter(form);
+			axios.get(origin, { params: data })
 				.then((response) => {
-					console.log(response);
+					console.log(response); // eslint-disable-line no-console
 				})
 				.catch((error) => {
-					console.log(error);
+					console.log(error); // eslint-disable-line no-console
 				});
 		});
 	}
@@ -37,13 +37,13 @@ export default class Form extends Component {
 			<form className="pedido" name="submit-to-google-sheet">
 				<input name="nome" type="text" placeholder="nome" required />
 				<input name="email" type="email" placeholder="Email" required />
-				{/* <input name="rua" type="text" placeholder="Rua" required />
+				<input name="rua" type="text" placeholder="Rua" required />
 				<input name="numero" type="text" placeholder="Número" required />
 				<input name="complemento" type="text" placeholder="Complemento" required />
 				<input name="bairro" type="text" placeholder="Bairro" required />
 				<input name="cep" type="text" placeholder="CEP" required />
 				<input name="cidade" type="text" placeholder="Cidade" required />
-				<input name="estado" type="text" placeholder="Estado" required /> */}
+				<input name="estado" type="text" placeholder="Estado" required />
 				<button type="submit">Send</button>
 			</form>
 		);
